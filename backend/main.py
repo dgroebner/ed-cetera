0b5397795ebcccae176d046f7f4c04820600ac6f
Danny Gröbner <danny.groebner@gmail.com>
2026-08-28 18:48:57 +0200

    add ssl support

--- backend/main.py
+++ backend/main.py
@@ -95,4 +95,11 @@ async def get_status():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=5000,
+        reload=True,
+        ssl_keyfile="../certs/key.pem",
+        ssl_certfile="../certs/cert.pem"
+    )
